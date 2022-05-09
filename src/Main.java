eab9f97a503babc797003fb9d051b80b0b9aae86
AlexCabrera123 <100866047+AlexCabrera123@users.noreply.github.com>
2022-05-09 11:54:09 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        //ToDo Clase padre Figura, va a ser abstracta
+        //color string, relleno booleano
+        //constructor para los dos parametros
+        //getters y setters
+        //obtenerArea -> abstracto, que retorne doble
+        //obtenerPerimetro -> abstracto, que retorne doble
+
+
+        //ToDo Circulo
+        //radio double
+        //constructor hereda super, e incluye el radio
+        //getters y setters
+        //implememtar obtenerArea
+        //implementar obtenerPerimetro
+
+
+        //ToDo Rectangulo
+        //ancho double, alto double
+        //constructor hereda super
+        //getters y setters
+        //implementar obtenerArea
+        //implementar obtenerPerimetro
+
+
+    }
+}
